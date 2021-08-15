f9c084a2eb79c1fc254325feb645b05b027a9113
Aleksandra Dubrovskaya <sash.dubrovskaya@yandex.ru>
2021-08-16 01:43:25 +0300

    second iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,31 @@
-let editPopupButton = document.querySelector('.edit-button');
+let editPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closePopupButton = document.querySelector('.close-button');
+let closePopupButton = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_job');
 let formElement = document.querySelector('.popup__form');
-let submitButton = document.querySelector('.submit-button');
 
-function togglePopup() {
+function openPopup() {
   if (!popup.classList.contains('popup_opened')) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
-  } else {
-    nameInput.value != profileName.textContent;
-    jobInput.value != profileJob.textContent;
   }
-  popup.classList.toggle('popup_opened');
+  popup.classList.add('popup_opened');
 }
 
-editPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
 function formSubmitHandler(event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  togglePopup();
+  closePopup();
 }
 
+editPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
